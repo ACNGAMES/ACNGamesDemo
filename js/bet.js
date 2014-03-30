0c81773b480621e759935ea2c394408d31e6fa79
unknown <ezequiel.ortiz@.dir.svc.accenture.com>
2014-03-30 01:56:58 -0300

    seagrego el balck jack y se adapto para que funcione con el credito del juego

---
+++ js/bet.js
@@ -0,0 +1,10 @@
+function makeBet(amount, descr){
+	console.log(descr+" "+amount);
+	reloadCredit();
+}
+
+function payBet(amount,descr){
+	console.log(descr+" "+amount);
+	reloadCredit();
+}
+
--- js/estructure.front.js
+++ js/estructure.front.js
@@ -9,3 +9,7 @@ function loadPageEstruct(){
 };
 
 
+function getGames(){
+	playBlackJack();
+	
+};
